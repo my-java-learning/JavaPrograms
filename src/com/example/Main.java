9b2413c01e6a730a58d0d6c8d2131ac0ada9f36d
Qsior <lukaszbojes@gmail.com>
2017-12-30 01:43:52 +0100

    Added examples of autoboxing java primitives

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,8 +1,61 @@
 package com.example;
 
+import java.util.ArrayList;
+
+class IntClass {
+    private int myValue;
+
+    public IntClass(int myValue) {
+        this.myValue = myValue;
+    }
+
+    public int getMyValue() {
+        return myValue;
+    }
+
+    public void setMyValue(int myValue) {
+        this.myValue = myValue;
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String[] strArray = new String[10];
+        int[] intArray = new int[10];
+
+        ArrayList<String> strArrayList = new ArrayList<String>();
+        strArrayList.add("Tim");
+
+//        ArrayList<int> intArrayList = new ArrayList<int>();
+        ArrayList<IntClass> intClassArrayList = new ArrayList<IntClass>();
+        intClassArrayList.add(new IntClass(54));
+        Integer integer = new Integer(54);
+        Double doubleValue = new Double(12.25);
+        ArrayList<Integer> intArrayList = new ArrayList<Integer>();
+
+        for(int i = 0 ; i < 10 ; i++){
+            intArrayList.add(Integer.valueOf(i));
+        }
+
+        for(int i = 0 ; i < 10 ; i++){
+            System.out.println(i + " --> " + intArrayList.get(i).intValue());
+        }
+
+        Integer myIntValue = 65; //Integer.valueOf(56);
+        int myInt = myIntValue; // myIntValue.intValue();
+
+        ArrayList<Double> myDoubleValues = new ArrayList<Double>();
+
+        for (double i = 0.0 ; i <= 10.0 ; i+=0.5){
+            //myDoubleValues.add(Double.valueOf(i));
+            myDoubleValues.add(i);
+        }
+
+        for (int i = 0; i < myDoubleValues.size(); i++){
+//            System.out.println(i + " ---> " + myDoubleValues.get(i).doubleValue());
+            System.out.println(i + " ---> " + myDoubleValues.get(i));
+        }
+
     }
 }
